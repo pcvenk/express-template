de18a6e9047f2309e0f41648c02db3f583d2f3d6
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-22 10:12:40 +0100

    Initial commit for the express template serving a static website.

---
+++ run.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+const PORT = 3000;
+
+app.use('/', express.static(path.join(__dirname, '/public/html')));
+
+app.listen(PORT, function(){
+   console.log('Server started on port 3000');
+});
+
