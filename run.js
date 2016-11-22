61c1735ce780c7a8cb542f4fb307cdd685be7de8
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-11-22 11:40:36 +0100

    Added the redicrect routes.

--- run.js
+++ run.js
@@ -7,6 +7,41 @@ const PORT = 3000;
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
+app.get('/employees', function(req, res){
+
+   var employees = [
+      {
+         firstName: 'John',
+         lastName: 'Doe',
+         age: 32,
+         countryOfResidence: 'USA'
+      },
+      {
+         firstName: 'Jane',
+         lastName: 'Doe',
+         age: 22,
+         countryOfResidence: 'USA'
+      },
+      {
+         firstName: 'Mark',
+         lastName: 'Smith',
+         age: 43,
+         countryOfResidence: 'Canada'
+      }
+   ];
+
+   res.json(employees);
+
+});
+
+app.get('/about', function(req, res){
+   res.redirect('/about.html');
+});
+
+app.get('/services', function(req, res){
+   res.redirect('/services.html');
+});
+
 app.listen(PORT, function(){
    console.log('Server started on port 3000');
 });
